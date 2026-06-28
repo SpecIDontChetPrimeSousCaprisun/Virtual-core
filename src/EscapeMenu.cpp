d360f975bf37b751b9fabb134873ae3208fadb30
SpecIDontChetPrimeSousCaprisun <corieux1@gmail.com>
2026-06-28 20:53:04 +0200

    Progress on the escape menu

--- src/EscapeMenu.cpp
+++ src/EscapeMenu.cpp
@@ -1,5 +1,6 @@
 #include "EscapeMenu.h"
 #include "UIElement.h"
+#include "Button.h"
 #include "Window.h"
 
 Container* EscapeMenu::ui;
@@ -11,6 +12,50 @@ void EscapeMenu::init() {
 
   UIElement* background = new UIElement(glm::vec2(0.0f, 0.0f), glm::vec2(1.0f, 1.0f), 0.1f, glm::vec3(0.0f, 0.0f, 0.0f), 100);
 
+  Button* tutorialsButton = new Button(glm::vec2(0.0f, 0.675f),
+                                  glm::vec2(1.0f, 0.075f),
+                                  1.0f,
+                                  glm::vec3(0.0f, 0.0f, 0.0f),
+                                  101,
+                                  "Tutorials",
+                                  "fonts/JetBrainsMonoNerdFont-Regular.ttf",
+                                  glm::vec3(1.0f, 1.0f, 1.0f));
+
+  Button* quitButton = new Button(glm::vec2(0.0f, 0.75f),
+                                  glm::vec2(1.0f, 0.075f),
+                                  1.0f,
+                                  glm::vec3(0.0f, 0.0f, 0.0f),
+                                  101,
+                                  "Quit",
+                                  "fonts/JetBrainsMonoNerdFont-Regular.ttf",
+                                  glm::vec3(1.0f, 1.0f, 1.0f));
+
+  tutorialsButton->textCentered = false;
+  tutorialsButton->setHoverCallback([tutorialsButton](bool hovered) {
+    if (hovered) {
+      tutorialsButton->text = "$ Tutorials";
+    } else {
+      tutorialsButton->text = "Tutorials";
+    }
+  });
+  tutorialsButton->setCallback([]() {
+    
+  });
+
+  quitButton->textCentered = false;
+  quitButton->setHoverCallback([quitButton](bool hovered) {
+    if (hovered) {
+      quitButton->text = "$ Quit";
+    } else {
+      quitButton->text = "Quit";
+    }
+  });
+  quitButton->setCallback([]() {
+    Window::inGame = false;
+  });
+
+  uiElements.push_back(tutorialsButton);
+  uiElements.push_back(quitButton);
   uiElements.push_back(background);
 
   ui = new Container(uiElements);
