21606cdb81140db1ea1f2461c309e26ea2bf79fe
SpecIDontChetPrimeSousCaprisun <corieux1@gmail.com>
2026-06-29 21:51:18 +0200

    Removed the comments in Intro.cpp

--- src/Intro.cpp
+++ src/Intro.cpp
@@ -192,31 +192,7 @@ void Intro::init() {
   startButton->setCallback([]() {
     ui->changeVisibility(false);
     background->visible = false;
-    /*
-    Player* plr = new Player(glm::vec2(0.0f, 0.0f), glm::vec2(100.0f, 100.0f), 0.0f, "textures/player.png", 2, true);
-    Object* obj2 = new Object(glm::vec2(300.0f, 0.0f), glm::vec2(100.0f, 10000.0f), 0.0f, "textures/box.png", 2);
-    Object* platform = new Object(glm::vec2(-500.0f, 500.0f), glm::vec2(1000.0f, 1000.0f), 0.0f, "textures/Wallpaper.jpeg", 1);
-    Object* background = new Object(glm::vec2(-750.0f, 250.0f), glm::vec2(1500.0f, 1500.0f), 0.0f, "textures/Wallpaper.jpeg", -1);
-    Item* item = new Item(glm::vec2(0.0f, 0.0f), glm::vec2(100.0f, 50.0f), 0.0f, "textures/player.png", 1, "test");
-    Item* item2 = new Item(glm::vec2(150.0f, 0.0f), glm::vec2(100.0f, 50.0f), 0.0f, "textures/player.png", 1, "test");
-
-    plr->registerObject();
-
-    //obj2->anchored = false;
-    obj2->canCollide = true;
-    obj2->linearVelocity = glm::vec2(-100.f, 0.0f);
-    obj2->rotation = 0;
-    obj2->registerObject();
-
-    platform->canCollide = true;
-    //platform->rotation = 15;
-    platform->registerObject();
-
-    background->parallaxFactor = 5.0f;
-    background->registerObject();
-
-    item->registerObject();
-    item2->registerObject();*/
+    
     LevelLoader::loadLevel("levels/testLevel");
   });
 
